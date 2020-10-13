dc5dfccd522eb478454f45a5492a798ed6223a6a
adriana <atoledosantiago1@gmail.com>
2020-10-13 15:40:04 -0500

    mvp day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,23 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # Constructor
+        self.reg = [0] * 8 # eight general purpose registers
+        self.ram = [0] * 256 # inital ram to hold 256 bytes of memory
+        self.pc = 0 # program counter - address of currently executing instruction
+
+        self.running = False
+
+    # RAM methods 
+     # Memory Address Register  -- address or key we are writing value to
+    # address we are looking at and it is like a key in ram that returns the values at that address
+    
+    def ram_read(self, address):
+        return self.ram[address]
+
+# what is being written to the value
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +78,31 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        running = True
+    # ir is instruction register
+    # pc is the counter  points us to memory location of ram
+        while running:
+
+            ir = self.ram_read(self.pc)  # current instruction
+            operand_a = self.ram_read(self.pc + 1)  # store bytes for a at pc + 1
+            operand_b = self.ram_read(self.pc + 2)  # store bytes for b at pc + 2
+
+            if ir == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif ir == PRN:
+                print_item = self.ram[self.pc + 1]
+                print(self.reg[print_item])
+                self.pc += 2
+
+            elif ir == HLT:  # halt function
+                running = False
+
+            else:
+                print('Not working')
+                running = False
