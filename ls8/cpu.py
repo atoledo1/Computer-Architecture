50c2e5285880a8ad694ba5a19ea789e57442c5f8
adriana <atoledosantiago1@gmail.com>
2020-10-14 17:52:28 -0500

    day 2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,58 +1,72 @@
 """CPU functionality."""
-
 import sys
+
+# set instructions in binary
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+
 
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        # Constructor
         self.reg = [0] * 8 # eight general purpose registers
         self.ram = [0] * 256 # inital ram to hold 256 bytes of memory
-        self.pc = 0 # program counter - address of currently executing instruction
-
-        self.running = False
-
-    # RAM methods 
-     # Memory Address Register  -- address or key we are writing value to
-    # address we are looking at and it is like a key in ram that returns the values at that address
-    
-    def ram_read(self, address):
-        return self.ram[address]
-
-# what is being written to the value
+        self.pc = 0  # PROGRAM COUNTER
+        self.ir = 0  # INSTRUCTION REGISTER
+        self.mar = 0  # MEMORY ADDRESS REGISTER
     def ram_write(self, value, address):
+        """
+        Writes the value to the address passed in.
+        Gets the address through the pc register
+        """
         self.ram[address] = value
-
+    def ram_read(self, address):
+        """ 
+        Accept the address to read and returns the value stored there.
+        Get the address through the pc register.
+        """
+        return self.ram[address]
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
+        if len(sys.argv) != 2:
+            print("usage: ls8.py filename")
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line = line.strip()  # strip deletes empty line/space when printing
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    if line == "" or line[0] == "#":
+                        continue  # pushes program through if line is empty
 
-
+                    try:
+                        str_value = line.split("#")[0]
+                        value = int(str_value, 2)  # BASE 2, BECAUSE its BINARY
+                    except ValueError:
+                        print(f"Invalid number: {str_value}")
+                        sys.exit(1)
+                    self.ram[address] = value
+                    address += 1
+        except FileNotFoundError:
+            print(f"File not found: {sys.argv[1]}")
+            sys.exit(2)
+            
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -61,48 +75,75 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
-
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
-
         print()
-
     def run(self):
         """Run the CPU."""
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
-
-        running = True
-    # ir is instruction register
-    # pc is the counter  points us to memory location of ram
-        while running:
-
-            ir = self.ram_read(self.pc)  # current instruction
-            operand_a = self.ram_read(self.pc + 1)  # store bytes for a at pc + 1
-            operand_b = self.ram_read(self.pc + 2)  # store bytes for b at pc + 2
-
-            if ir == LDI:
+        halted = False
+        # while running, get instruction from program counter address memory
+        while not halted:
+            instruction = self.ram[self.pc]
+            # get next 2 bytes of data as placeholders for instruction to use
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # check for different instruction cases
+            if instruction == HLT:  # Halt instruction, exits emulator
+                halted = True
+                self.pc = 0  # set steps counter back to zero
+            elif instruction == LDI:  # set value of the register to an integer
+                # operand_a is the register number
+                # operand_b is the value to set the register to
                 self.reg[operand_a] = operand_b
                 self.pc += 3
-
-            elif ir == PRN:
-                print_item = self.ram[self.pc + 1]
-                print(self.reg[print_item])
+            elif instruction == PRN:  # prints numeric value stored in given register
+                # operand_a is the register number
+                print(self.reg[operand_a])
                 self.pc += 2
-
-            elif ir == HLT:  # halt function
-                running = False
-
+            elif instruction == MUL:  # Multiplies reg_a * reg_b
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
             else:
-                print('Not working')
-                running = False
+                print(f"Instruction '{instruction}' not found")    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
